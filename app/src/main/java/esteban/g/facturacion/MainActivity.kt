afcb74aac44db817b784870240508845376839c4
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2023-12-29 12:04:08 -0500

    v0

--- app/src/main/java/esteban/g/facturacion/MainActivity.kt
+++ app/src/main/java/esteban/g/facturacion/MainActivity.kt
@@ -2,10 +2,22 @@ package esteban.g.facturacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val usernameEditText = findViewById<EditText>(R.id.username)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener{
+
+        }
     }
 }
